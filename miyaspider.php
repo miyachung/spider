6ad1466734d8875938b718c360b2add8ba563ba5
Miyachung <miyachung@hotmail.com>
2020-07-24 00:20:17 +0300

    Update miyaspider.php

--- miyaspider.php
+++ miyaspider.php
@@ -144,7 +144,7 @@ function header_reflect_check ( $content ){
 
 function form_contain_check( $content ){
 
-    if(preg_match_all('@<form(.*?)</form>@si',$content,$forms)){
+    if(preg_match('@<form(.*?)</form>@si',$content,$forms)){
         return $forms[0];
     }else{
         return false;
@@ -204,7 +204,10 @@ function internal_search( $links , $target_http ,$target ){
         }
         
         $check_form   = form_contain_check($spider_internal[0]);
-        if($check_form != false) write_to_file('internal.html','<ul><li><h4 style="margin:0;padding:0;">This page contains form</h4><br/><textarea style="width:50%;height:300px;overflow:auto">'.implode("\r\n",$check_form).'</textarea></li></ul>');
+        if($check_form != false){
+            $check_form_string = addslashes($check_form);
+            write_to_file('internal.html','<ul><li><h4 style="margin:0;padding:0;">This page contains form</h4><br/><textarea style="width:50%;height:300px;overflow:auto">'.$check_form_string.'</textarea></li></ul>');
+        } 
         if(preg_match('@<title>(.*?)</title>@i',$spider_internal[0],$title)){
             print "\tTitle found => ".trim(strip_tags($title[1])).PHP_EOL;
             write_to_file('internal.html',"<ul><li><h4 style=\"margin:0;padding:0;\">Title</h4> $title[1]</li></ul>");
@@ -269,7 +272,10 @@ function internal_search( $links , $target_http ,$target ){
             }
     
             $check_form = form_contain_check($search_deep[0]);
-            if($check_form != false) write_to_file('internal.html','<ul><li><h4 style="margin:0;padding:0;">This page contains form</h4><br/><textarea style="width:50%;height:300px;overflow:auto">'.implode("\r\n",$check_form).'</textarea></li></ul>');
+            if($check_form != false){
+                $check_form_string = addslashes($check_form);
+                write_to_file('internal.html','<ul><li><h4 style="margin:0;padding:0;">This page contains form</h4><br/><textarea style="width:50%;height:300px;overflow:auto">'.$check_form_string.'</textarea></li></ul>');
+            } 
             if(preg_match('@<title>(.*?)</title>@',$search_deep[0],$deep_title)){
                 print "\tTitle taken: ".trim(strip_tags($deep_title[1])).PHP_EOL;
                 write_to_file('internal.html','<ul><h4 style="margin:0;padding:0;">Title</h4><li>'.$deep_title[1].'</li></ul>');
